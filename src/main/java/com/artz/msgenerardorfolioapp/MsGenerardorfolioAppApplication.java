31d492243ece30e525f4fb0e844c1d6fbc4783b8
Carlos <carloscaamal12@gmail.com>
2022-10-21 13:51:50 -0500

    primer commit

---
+++ src/main/java/com/artz/msgenerardorfolioapp/MsGenerardorfolioAppApplication.java
@@ -0,0 +1,13 @@
+package com.artz.msgenerardorfolioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGenerardorfolioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGenerardorfolioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artz/msgenerardorfolioapp/MsGenerardorfolioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artz.msgenerardorfolioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGenerardorfolioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
